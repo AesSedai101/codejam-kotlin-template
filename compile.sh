ac25b2e1bbef46ae65494785bed274e20dc25732
Elsabe Ros <ElsabeR@discovery.co.za>
2020-02-15 14:12:37 +0200

    Pipe output to file as well.

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 directory=$1
 
 kotlinc $directory/Solution.kt -include-runtime -d $directory/out.jar
-java -jar $directory/out.jar < $directory/input.txt
+java -jar $directory/out.jar < $directory/input.txt > $directory/output.txt
